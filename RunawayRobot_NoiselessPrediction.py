193c523b69d0cfa131806575b9648b2525934594
ASzwarc <szwarc.adam@gmail.com>
2019-06-20 12:58:18 +0200

    Implementation of prediction algorithm of robot position when there is no noise

--- RunawayRobot_NoiselessPrediction.py
+++ RunawayRobot_NoiselessPrediction.py
@@ -59,6 +59,7 @@ from Robot import *
 from math import *
 from Matrix import *
 import random
+import matplotlib.pyplot as plt
 
 
 # This is the function you have to write. The argument 'measurement' is a 
@@ -71,8 +72,52 @@ def estimate_next_pos(measurement, OTHER = None):
     """Estimate the next (x, y) position of the wandering Traxbot
     based on noisy (x, y) measurements."""
 
-    # You must return xy_estimate (x, y), and OTHER (even if it is None) 
-    # in this order for grading purposes.
+    # if not OTHER:
+    #     OTHER = []
+    # OTHER.append(measurement)
+    
+    # angles = []
+    # for i in range(1, len(OTHER)):
+    #     angles.append(atan2(OTHER[i][1] - OTHER[i - 1][1], OTHER[i][0] - OTHER[i - 1][0]))
+
+    # if len(OTHER) >= 2:
+    #     point1 = OTHER[-1]
+    #     point2 = OTHER[-2]
+    #     velocity = distance_between(point1, point2)
+    #     angle = atan2(point1[1] - point2[1], point1[0] - point2[0])
+    #     if len(angles) > 1:
+    #         diff_angles = []
+    #         for i in range(1, len(angles)):
+    #             diff_angles.append(angles[i] - angles[i - 1])
+    #         angle_correction = sum(diff_angles) / len(diff_angles)
+    #     else:
+    #         angle_correction = sum(angles) / len(angles)
+
+    #     xy_estimate = (point1[0] + velocity * cos(angle - angle_correction) - 0.2, point1[1] + velocity * sin(angle - angle_correction))
+    # else:
+    #     xy_estimate = OTHER[-1]
+    if not OTHER:
+        OTHER = []
+    avgdist = 0
+    avgdtheta = 0
+    OTHER.append(measurement)
+    dist = []
+    theta = []
+    if len(OTHER) > 1:
+        for i in range(len(OTHER)-1):
+            dist.append(distance_between(OTHER[i+1],OTHER[i]))
+            theta.append(atan2(OTHER[i+1][1]-OTHER[i][1], OTHER[i+1][0]-OTHER[i][0]))
+    else:
+        dist = [0]
+        theta = [0]
+    avgdist = sum(dist) / float(len(dist))
+    dtheta = []
+    if len(theta) > 1:
+        for i in range(len(theta)-1):
+            dtheta.append((theta[i+1]-theta[i])%(2*pi))
+        avgdtheta = sum(dtheta) / float(len(dtheta))
+
+    xy_estimate = (measurement[0]+avgdist*cos(theta[len(theta)-1]+avgdtheta), measurement[1]+avgdist*sin(theta[len(theta)-1]+avgdtheta))
     return xy_estimate, OTHER 
 
 # A helper function you may find useful.
@@ -99,12 +144,72 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
         target_bot.move_in_circle()
         true_position = (target_bot.x, target_bot.y)
         error = distance_between(position_guess, true_position)
+        print("CTR: {} GUESS: {} TRUE POSITION: {}".format(ctr, position_guess, true_position))
+        plt.plot(true_position[0], true_position[1], 'bs', position_guess[0], position_guess[1], 'g^')
         if error <= distance_tolerance:
             print("You got it right! It took you {} steps to localize.".format(ctr))
             localized = True
         if ctr == 10:
             print("Sorry, it took you too many steps to localize the target.")
+    plt.show()
     return localized
+
+def demo_grading_with_visualization(estimate_next_pos_fcn, target_bot, OTHER = None):
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    #For Visualization
+    import turtle    #You need to run this locally to use the turtle module
+    window = turtle.Screen()
+    window.bgcolor('white')
+    size_multiplier= 25.0  #change Size of animation
+    broken_robot = turtle.Turtle()
+    broken_robot.shape('turtle')
+    broken_robot.color('green')
+    broken_robot.resizemode('user')
+    broken_robot.shapesize(0.1, 0.1, 0.1)
+    measured_broken_robot = turtle.Turtle()
+    measured_broken_robot.shape('circle')
+    measured_broken_robot.color('red')
+    measured_broken_robot.resizemode('user')
+    measured_broken_robot.shapesize(0.1, 0.1, 0.1)
+    prediction = turtle.Turtle()
+    prediction.shape('arrow')
+    prediction.color('blue')
+    prediction.resizemode('user')
+    prediction.shapesize(0.1, 0.1, 0.1)
+    prediction.penup()
+    broken_robot.penup()
+    measured_broken_robot.penup()
+    #End of Visualization
+    while not localized and ctr <= 100:
+        ctr += 1
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        if error <= distance_tolerance:
+            print("You got it right! It took you {} steps to localize.".format(ctr))
+            localized = True
+        if ctr == 10:
+            print("Sorry, it took you too many steps to localize the target.")
+        #More Visualization
+        measured_broken_robot.setheading(target_bot.heading*180/pi)
+        measured_broken_robot.goto(measurement[0]*size_multiplier, measurement[1]*size_multiplier-200)
+        measured_broken_robot.stamp()
+        broken_robot.setheading(target_bot.heading*180/pi)
+        broken_robot.goto(target_bot.x*size_multiplier, target_bot.y*size_multiplier-200)
+        broken_robot.stamp()
+        prediction.setheading(target_bot.heading*180/pi)
+        prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
+        prediction.stamp()
+        #End of Visualization
+    return localized
+
 
 # This is a demo for what a strategy could look like. This one isn't very good.
 def naive_next_pos(measurement, OTHER = None):
@@ -121,4 +226,5 @@ def naive_next_pos(measurement, OTHER = None):
 test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
 test_target.set_noise(0.0, 0.0, 0.0)
 
-# demo_grading(naive_next_pos, test_target)
+demo_grading(estimate_next_pos, test_target)
+#demo_grading_with_visualization(estimate_next_pos, test_target)
